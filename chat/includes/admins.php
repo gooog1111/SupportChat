f01e8140f4dec18dfd53a541caaa2db8138372ae
Ivan Kiselev <kiselev.ivan1111@gmail.com>
2025-03-22 23:11:01 +0300

    Add files via upload

---
+++ chat/includes/admins.php
@@ -0,0 +1,12 @@
+<?php $ADMINS = array (
+  'admin' => 
+  array (
+    'password' => '$2y$10$DCjIXIdp9qF.HwZAQhkH8OUMtGuKcAyYjFDxSWPyn4OhkecGFCo4S',
+    'name' => 'admin1',
+  ),
+  'admin1' => 
+  array (
+    'password' => '$2y$10$DCjIXIdp9qF.HwZAQhkH8OUMtGuKcAyYjFDxSWPyn4OhkecGFCo4S',
+    'name' => 'admin2',
+  ),
+); ?>
